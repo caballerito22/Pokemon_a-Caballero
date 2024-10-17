67983dc9932f9513f30cc099ed18979e337220ef
caballero22A <118520457+caballero22A@users.noreply.github.com>
2024-10-17 17:06:59 +0200

    3.5

--- app/src/main/java/com/example/pokemon_a_caballero/PokemonAdapter.java
+++ app/src/main/java/com/example/pokemon_a_caballero/PokemonAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,9 +20,17 @@ public class PokemonAdapter extends ArrayAdapter<Pokemon> {
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         Pokemon pokemon = getItem(position);
 
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        convertView = inflater.inflate(R.layout.pokemon_list_item, parent, false);
+
+        TextView textoPokemonName = convertView.findViewById(R.id.textoPokemonName);
+        TextView textoPokemonSpecie = convertView.findViewById(R.id.textoPokemonSpecie);
+
+        textoPokemonName.setText(pokemon.getName());
+        textoPokemonSpecie.setText(pokemon.getSpecies());
 
 
 
-        return convertView;
+        return super.getView(position, convertView, parent);
     }
 }
