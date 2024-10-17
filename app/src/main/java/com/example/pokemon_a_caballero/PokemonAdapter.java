f7621058f5817e677bb0119cb516414cce7d3fc9
alecabnun <avestruz2265@gmail.com>
2024-10-17 13:04:52 +0200

    3.5si

--- app/src/main/java/com/example/pokemon_a_caballero/PokemonAdapter.java
+++ app/src/main/java/com/example/pokemon_a_caballero/PokemonAdapter.java
@@ -1,4 +1,27 @@
 package com.example.pokemon_a_caballero;
 
-public class PokemonAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class PokemonAdapter extends ArrayAdapter<Pokemon> {
+    public PokemonAdapter(@NonNull Context context, int resource, @NonNull Pokemon[] objects) {
+        super(context, resource, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Pokemon pokemon = getItem(position);
+
+
+
+
+        return convertView;
+    }
 }
