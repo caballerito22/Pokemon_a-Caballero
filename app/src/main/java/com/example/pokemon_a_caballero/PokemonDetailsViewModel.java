aa3a150cd21c2da3a67f02051cd5d87376dd1f39
caballero22A <118520457+caballero22A@users.noreply.github.com>
2024-10-18 17:02:52 +0200

    3.5

---
+++ app/src/main/java/com/example/pokemon_a_caballero/PokemonDetailsViewModel.java
@@ -0,0 +1,7 @@
+package com.example.pokemon_a_caballero;
+
+import androidx.lifecycle.ViewModel;
+
+public class PokemonDetailsViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/pokemon_a_caballero/pokemonDetailsFragment.java
@@ -0,0 +1,45 @@
+package com.example.pokemon_a_caballero;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class pokemonDetailsFragment extends Fragment {
+
+    private PokemonDetailsViewModel mViewModel;
+
+    public static pokemonDetailsFragment newInstance() {
+        return new pokemonDetailsFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        Bundle args = getArguments();
+
+        if (args != null){
+            Pokemon pokemon = (Pokemon) args.getSerializable("pokemon");
+            Log.d("XXX",pokemon.toString());
+        }
+
+        return inflater.inflate(R.layout.fragment_pokemon_details, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(PokemonDetailsViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
