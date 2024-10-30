0aa60cddb8fc50faa97b68fa0f3308d6efdab4cd
alecabnun <avestruz2265@gmail.com>
2024-10-30 13:35:23 +0100

    settings listo

---
+++ app/src/main/java/com/example/pokemon_a_caballero/AppDatabase.java
@@ -0,0 +1,12 @@
+package com.example.pokemon_a_caballero;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Pokemon.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    private static AppDatabase INSTANCE;
+
+    public static AppDatabase getDatabase(C)
+
+}
